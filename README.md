a7d48e88c5c47e869156b4ab4e115530fdd477a5
Nathan Buesgens <nathan@natb1.com>
2014-05-01 21:40:30 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+```shell
+pip install git+https://github.com/natb1/mapping_tools.git
+```
+... a collection of domain models and domain objects for testing purposes
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='test_data',
+    description='a colection of domain models and domain objects for testing purposes',
+    long_description=open('README.md').read(),
+    url='https://github.com/natb1/test_data',
+    author='Nathan Buesgens',
+    author_email='nathan@natb1.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7'
+    ],
+    keywords='development',
+    packages=['test_data', 'test_data.zoo],
+)
